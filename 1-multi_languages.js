52bd3288cca674186d0cfd21f6cbf540f0bc9717
Obeng Josiah <josiahobeng87@gmail.com>
2025-05-29 13:50:07 +0000

    new 3 code lines

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,7 +1,6 @@
-const lines1 = "C is fun";
-const lines2 = "Phython is cool";
-const lines3 = "Javascript is amazing";
-
+const line1 = "C is fun";
+const line2 = "Python is cool";
+const line3 = "JavaScript is amazing";
 
 console.log(line1);
 console.log(line2);
